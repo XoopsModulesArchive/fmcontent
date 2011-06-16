e82594778dbfbf9568f34cf3ce1e6e7f8c76eb38
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-06-16 08:49:58 +0000

    Reformat codes

--- XoopsModules/fmcontent/trunk/xoops_lib/modules/fmcontent/submit.php
+++ XoopsModules/fmcontent/trunk/xoops_lib/modules/fmcontent/submit.php
@@ -18,103 +18,104 @@
  * @version     $Id:$
  */
 
-if (!isset($forMods)) exit('Module not found');
+if (! isset ( $forMods ))
+	exit ( 'Module not found' );
 
 include_once XOOPS_ROOT_PATH . "/class/xoopsformloader.php";
 include_once XOOPS_ROOT_PATH . "/class/tree.php";
 
-$op = fmcontent_CleanVars($_REQUEST, 'op', '', 'string');
+$op = fmcontent_CleanVars ( $_REQUEST, 'op', '', 'string' );
 
 // include Xoops header
 include XOOPS_ROOT_PATH . '/header.php';
 
-$content_handler = xoops_getmodulehandler('page', 'fmcontent');
-$topic_handler = xoops_getmodulehandler('topic', 'fmcontent');
+$content_handler = xoops_getmodulehandler ( 'page', 'fmcontent' );
+$topic_handler = xoops_getmodulehandler ( 'topic', 'fmcontent' );
 
 // Include language file
-xoops_loadLanguage('admin', $forMods->getVar('dirname', 'e'));
+xoops_loadLanguage ( 'admin', $forMods->getVar ( 'dirname', 'e' ) );
 
 // Check the access permission
 global $xoopsUser;
-$perm_handler = fmcontentPermission::getHandler();
-if (!$perm_handler->isAllowed($xoopsUser, 'fmcontent_ac', '8', $forMods)) {
-    redirect_header("index.php", 3, _NOPERM);
-    exit;
+$perm_handler = fmcontentPermission::getHandler ();
+if (! $perm_handler->isAllowed ( $xoopsUser, 'fmcontent_ac', '8', $forMods )) {
+	redirect_header ( "index.php", 3, _NOPERM );
+	exit ();
 }
 
 switch ($op) {
-    case 'add':
-
-        if (!isset($_POST['post'])) {
-            redirect_header("index.php", 3, _NOPERM);
-            exit;
-        }
-
-        if ($_REQUEST['content_modid'] != $forMods->getVar('mid')) {
-            redirect_header('index.php', 3, _NOPERM);
-            exit();
-        }
-
-        $groups = xoops_getModuleOption('groups', $forMods->getVar('dirname'));
-        $groups = (isset($groups)) ? $groups : '';
-        $groups = (is_array($groups)) ? implode(" ", $groups) : '';
-
-        $obj = $content_handler->create();
-        $obj->setVars($_REQUEST);
-
-        $criteria = new CriteriaCompo();
-        $criteria->add(new Criteria('content_modid', $forMods->getVar('mid')));
-        $criteria->setSort('content_order');
-        $criteria->setOrder('DESC');
-        $criteria->setLimit(1);
-        $last = $content_handler->getObjects($criteria);
-        $order = 1;
-        foreach ($last as $item) {
-            $order = $item->getVar('content_order') + 1;
-        }
-
-        $obj->setVar('content_order', $order);
-
-        $obj->setVar('content_menu', fmcontent_AjaxFilter($_REQUEST['content_title']));
-        $obj->setVar('content_alias', fmcontent_Filter($_REQUEST['content_title']));
-        $obj->setVar('content_words', fmcontent_MetaFilter($_REQUEST['content_title']));
-        $obj->setVar('content_desc', fmcontent_AjaxFilter($_REQUEST['content_title']));
-
-        $obj->setVar('content_create', time());
-        $obj->setVar('content_update', time());
-        $obj->setVar('content_groups', $groups);
-
-        //Form topic_img
-        fmcontentUtils::uploadimg($forMods, 'content_img' , $obj,$_REQUEST['content_img']);
-
-        if ($perm_handler->isAllowed($xoopsUser, 'fmcontent_ac', '16', $forMods)) {
-            $obj->setVar('content_status', '1');
-            $content_handler->updateposts($_REQUEST['content_uid'], '1', $content_action = 'add');
-        }
-
-        if (!$content_handler->insert($obj)) {
-            fmcontent_Redirect('onclick="javascript:history.go(-1);"', 1, _FMCONTENT_MSG_ERROR);
-            include XOOPS_ROOT_PATH . '/footer.php';
-            exit();
-        }
-
-        if ((xoops_getModuleOption('usetag', $forMods->getVar('dirname'))) and (is_dir(XOOPS_ROOT_PATH . '/modules/tag'))) {
-            $tag_handler = xoops_getmodulehandler('tag', 'tag');
-            $tag_handler->updateByItem($_POST["item_tag"], $obj->getVar('content_id'), $forMods->getVar("dirname"), 0);
-        }
-
-        // Redirect page
-        fmcontent_Redirect('index.php', 1, _FMCONTENT_MSG_WAIT);
-        include XOOPS_ROOT_PATH . '/footer.php';
-        exit();
-        break;
-
-    default:
-
-        $content_type = fmcontent_CleanVars($_REQUEST, 'content_type', 'content', 'string');
-        $obj = $content_handler->create();
-        $obj->getContentSimpleForm($forMods, $content_type);
-        break;
+	case 'add' :
+		
+		if (! isset ( $_POST ['post'] )) {
+			redirect_header ( "index.php", 3, _NOPERM );
+			exit ();
+		}
+		
+		if ($_REQUEST ['content_modid'] != $forMods->getVar ( 'mid' )) {
+			redirect_header ( 'index.php', 3, _NOPERM );
+			exit ();
+		}
+		
+		$groups = xoops_getModuleOption ( 'groups', $forMods->getVar ( 'dirname' ) );
+		$groups = (isset ( $groups )) ? $groups : '';
+		$groups = (is_array ( $groups )) ? implode ( " ", $groups ) : '';
+		
+		$obj = $content_handler->create ();
+		$obj->setVars ( $_REQUEST );
+		
+		$criteria = new CriteriaCompo ();
+		$criteria->add ( new Criteria ( 'content_modid', $forMods->getVar ( 'mid' ) ) );
+		$criteria->setSort ( 'content_order' );
+		$criteria->setOrder ( 'DESC' );
+		$criteria->setLimit ( 1 );
+		$last = $content_handler->getObjects ( $criteria );
+		$order = 1;
+		foreach ( $last as $item ) {
+			$order = $item->getVar ( 'content_order' ) + 1;
+		}
+		
+		$obj->setVar ( 'content_order', $order );
+		
+		$obj->setVar ( 'content_menu', fmcontent_AjaxFilter ( $_REQUEST ['content_title'] ) );
+		$obj->setVar ( 'content_alias', fmcontent_Filter ( $_REQUEST ['content_title'] ) );
+		$obj->setVar ( 'content_words', fmcontent_MetaFilter ( $_REQUEST ['content_title'] ) );
+		$obj->setVar ( 'content_desc', fmcontent_AjaxFilter ( $_REQUEST ['content_title'] ) );
+		
+		$obj->setVar ( 'content_create', time () );
+		$obj->setVar ( 'content_update', time () );
+		$obj->setVar ( 'content_groups', $groups );
+		
+		//Form topic_img
+		fmcontentUtils::uploadimg ( $forMods, 'content_img', $obj, $_REQUEST ['content_img'] );
+		
+		if ($perm_handler->isAllowed ( $xoopsUser, 'fmcontent_ac', '16', $forMods )) {
+			$obj->setVar ( 'content_status', '1' );
+			$content_handler->updateposts ( $_REQUEST ['content_uid'], '1', $content_action = 'add' );
+		}
+		
+		if (! $content_handler->insert ( $obj )) {
+			fmcontent_Redirect ( 'onclick="javascript:history.go(-1);"', 1, _FMCONTENT_MSG_ERROR );
+			include XOOPS_ROOT_PATH . '/footer.php';
+			exit ();
+		}
+		
+		if ((xoops_getModuleOption ( 'usetag', $forMods->getVar ( 'dirname' ) )) and (is_dir ( XOOPS_ROOT_PATH . '/modules/tag' ))) {
+			$tag_handler = xoops_getmodulehandler ( 'tag', 'tag' );
+			$tag_handler->updateByItem ( $_POST ["item_tag"], $obj->getVar ( 'content_id' ), $forMods->getVar ( "dirname" ), 0 );
+		}
+		
+		// Redirect page
+		fmcontent_Redirect ( 'index.php', 1, _FMCONTENT_MSG_WAIT );
+		include XOOPS_ROOT_PATH . '/footer.php';
+		exit ();
+		break;
+	
+	default :
+		
+		$content_type = fmcontent_CleanVars ( $_REQUEST, 'content_type', 'content', 'string' );
+		$obj = $content_handler->create ();
+		$obj->getContentSimpleForm ( $forMods, $content_type );
+		break;
 
 }
 
