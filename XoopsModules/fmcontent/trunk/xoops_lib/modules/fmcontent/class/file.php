0b55b4eb3e60b13794614f350d5dda62e6ce90b2
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-08-30 11:30:20 +0000

    Add Upload file

--- XoopsModules/fmcontent/trunk/xoops_lib/modules/fmcontent/class/file.php
+++ XoopsModules/fmcontent/trunk/xoops_lib/modules/fmcontent/class/file.php
@@ -28,11 +28,70 @@ class fmcontent_file extends XoopsObject {
 		$this->initVar ( "file_content", XOBJ_DTYPE_INT, '' );
 		$this->initVar ( "file_date", XOBJ_DTYPE_INT, '' );
 		$this->initVar ( "file_type", XOBJ_DTYPE_TXTBOX, '' );
-		$this->initVar ( "file_status", XOBJ_DTYPE_INT, '' );
+		$this->initVar ( "file_status", XOBJ_DTYPE_INT, 1 );
 
 		$this->db = $GLOBALS ['xoopsDB'];
 		$this->table = $this->db->prefix ( 'fmcontent_file' );
-	}	
+	}
+	
+	function getForm($forMods) {
+		$form = new XoopsThemeForm ( _FMCONTENT_FORM_FILE, 'file', 'backend.php', 'post' );
+		$form->setExtra ( 'enctype="multipart/form-data"' );
+		
+		if ($this->isNew ()) {
+			$form->addElement ( new XoopsFormHidden ( 'op', 'add_file' ) );
+		} else {
+			$form->addElement ( new XoopsFormHidden ( 'op', 'edit_file' ) );
+		}
+		$form->addElement ( new XoopsFormHidden ( 'file_id', $this->getVar ( 'file_id', 'e' ) ) );
+		$form->addElement ( new XoopsFormHidden ( 'file_modid', $forMods->getVar ( 'mid' ) ) );
+		$form->addElement ( new XoopsFormText ( _FMCONTENT_FILE_TITLE, "file_title", 50, 255, $this->getVar ( "file_title" ) ), true );
+		
+		$content_Handler = xoops_getModuleHandler ( "page", "fmcontent" );
+		$criteria = new CriteriaCompo ();
+		$criteria->add ( new Criteria ( 'content_modid', $forMods->getVar ( 'mid' ) ) );
+		$criteria->add ( new Criteria ( 'content_status', '1' ) );
+		$content = $content_Handler->getObjects ( $criteria );
+		$tree = new XoopsObjectTree ( $content, 'content_id', 'content_topic' );
+		ob_start ();
+		echo $tree->makeSelBox ( 'file_content', 'content_title', '', $this->getVar ( 'file_content', 'e' ), true );
+		$form->addElement ( new XoopsFormLabel ( _FMCONTENT_FILE_CONTENT, ob_get_contents () ) );
+		ob_end_clean ();
+		$form->addElement ( new XoopsFormRadioYN ( _FMCONTENT_STATUS, 'file_status', $this->getVar ( 'file_status', 'e' ) ) );
+		
+		if ($this->isNew ()) {
+		$uploadirectory_file = xoops_getModuleOption ( 'file_dir', $forMods->getVar ( 'dirname' ) );
+		$fileseltray_file = new XoopsFormElementTray ( _FMCONTENT_FILE, '<br />' );
+		$fileseltray_file->addElement ( new XoopsFormFile ( _FMCONTENT_SELECT_FILE, 'file_name', xoops_getModuleOption ( 'file_size', $forMods->getVar ( 'dirname' ) ) ), false );
+		$form->addElement ( $fileseltray_file );
+		}
+		// Submit buttons
+		$button_tray = new XoopsFormElementTray ( '', '' );
+		$submit_btn = new XoopsFormButton ( '', 'post', _SUBMIT, 'submit' );
+		$button_tray->addElement ( $submit_btn );
+		$cancel_btn = new XoopsFormButton ( '', 'cancel', _CANCEL, 'cancel' );
+		$cancel_btn->setExtra ( 'onclick="javascript:history.go(-1);"' );
+		$button_tray->addElement ( $cancel_btn );
+		$form->addElement ( $button_tray );
+		$form->display ();
+		
+		return $form;
+	}		
+	
+		/**
+	 * Returns an array representation of the object
+	 *
+	 * @return array
+	 **/
+	function toArray() {
+		$ret = array ();
+		$vars = $this->getVars ();
+		foreach ( array_keys ( $vars ) as $i ) {
+			$ret [$i] = $this->getVar ( $i );
+		}
+		return $ret;
+	}
+	
 }
 
 class fmcontentFileHandler extends XoopsPersistableObjectHandler {
@@ -41,5 +100,32 @@ class fmcontentFileHandler extends XoopsPersistableObjectHandler {
 		parent::XoopsPersistableObjectHandler ( $db, 'fmcontent_file', 'fmcontent_file', 'file_id', 'file_title' );
 	}
 
+	function getFiles($forMods, $file) {
+		$ret = array ();
+			$criteria = new CriteriaCompo ();
+			$criteria->add ( new Criteria ( 'file_modid', $forMods->getVar ( 'mid' ) ) );
+			$criteria->setSort ( $file['sort'] );
+			$criteria->setOrder ( $file['order'] );
+			$criteria->setLimit ( $file['limit'] );
+			$criteria->setStart ( $file['start'] );
+		
+		$files = $this->getObjects ( $criteria, false );
+		if ($files) {
+			
+			foreach ( $files as $root ) {
+				$tab = array ();
+				$tab = $root->toArray ();
+				$tab ['fileurl'] = XOOPS_URL . xoops_getModuleOption ( 'file_dir', $forMods->getVar ( 'dirname' ) ) . $root->getVar ( 'file_name' );
+				$ret [] = $tab;
+			}
+		}
+		return $ret;
+	}
+	
+	function getFileCount ($forMods) {
+		$criteria = new CriteriaCompo ();
+		$criteria->add ( new Criteria ( 'file_modid', $forMods->getVar ( 'mid' ) ) );
+		return $this->getCount ( $criteria );
+	}	
 }
 ?>
