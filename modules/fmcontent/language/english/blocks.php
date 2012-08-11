3d734b77fc22125660d49ef9d7085e030f1e66ad
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- modules/fmcontent/language/english/blocks.php
+++ modules/fmcontent/language/english/blocks.php
@@ -19,23 +19,23 @@ if (!defined('_FMCONTENT_TYPE')) {
     //Menu
     define("_MI_FMCONTENT_MENUTODISPLAY", "Select content type/topic to display");
     define("_MI_FMCONTENT_DISPLAY", "Display");
-    define('_MI_FMCONTENT_MENU_DEFAULT', 'Static pages');
-    define('_MI_FMCONTENT_TYPE', 'Menu template: ');
-    define('_MI_FMCONTENT_HORIZ', 'Horizontal');
-    define('_MI_FMCONTENT_VERT', 'Vertical');
-    define('_MI_FMCONTENT_SIMPLE', 'Default');
-    define('_MI_FMCONTENT_MENU_ALL', 'All Contents');
+    define('_MI_FMCONTENT_MENU_DEFAULT','Static pages');
+    define('_MI_FMCONTENT_TYPE','Menu template: ');
+    define('_MI_FMCONTENT_HORIZ','Horizontal');
+    define('_MI_FMCONTENT_VERT','Vertical');
+    define('_MI_FMCONTENT_SIMPLE','Default');
+    define('_MI_FMCONTENT_MENU_ALL','All Contents');
 //Page
-    define('_MI_FMCONTENT_SELECTPAGE', 'Select the page to display: ');
+    define('_MI_FMCONTENT_SELECTPAGE','Select the page to display: ');
 //List
-    define('_MI_FMCONTENT_ALLMENUS', 'All Topics');
-    define('_MI_FMCONTENT_TOPICDISPLAY', 'Select Topics to display');
-    define('_MI_FMCONTENT_NUMBER', 'Number of listed contents');
-    define('_MI_FMCONTENT_CHARS', 'Title lenght (characters)');
-    define('_MI_FMCONTENT_IMG', 'Display image');
-    define('_MI_FMCONTENT_WIDTH', 'Max image width (pixel)');
-    define('_MI_FMCONTENT_DESCRIPTION', 'Display content text');
-    define('_MI_FMCONTENT_DATE', 'Display publish date');
-    define('_MI_FMCONTENT_MORE', 'Read more');
+    define('_MI_FMCONTENT_ALLMENUS','All Topics');
+    define('_MI_FMCONTENT_TOPICDISPLAY','Select Topics to display');
+    define('_MI_FMCONTENT_NUMBER','Number of listed contents');
+    define('_MI_FMCONTENT_CHARS','Title lenght (characters)');
+    define('_MI_FMCONTENT_IMG','Display image');
+    define('_MI_FMCONTENT_WIDTH','Max image width (pixel)');
+    define('_MI_FMCONTENT_DESCRIPTION','Display content text');
+    define('_MI_FMCONTENT_DATE','Display publish date');
+    define('_MI_FMCONTENT_MORE','Read more');
 }
 ?>
